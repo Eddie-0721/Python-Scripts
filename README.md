c1dfa45598c0fc3ae1fc254508aecf915d8f3bef
WenPeng.OuYang <101856244+Eddie-0721@users.noreply.github.com>
2023-11-07 09:51:16 +0800

    添加注释

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ about python
 
 # remove_columns.py 20231106
 处理csv中的列（删除目录下所有csv中的第一列和第二列）。
+1. os.listdir(input_dir)：使用 os 模块的 listdir 函数列出指定目录 input_dir 中的所有文件和文件夹。
+2. file.endswith('.csv')：对于列出的每个文件名 file，使用 endswith 方法检查它是否以 .csv 结尾。如果是，则返回 True，否则返回 False。
+3. [file for file in os.listdir(input_dir) if file.endswith('.csv')]：使用列表推导式，将满足条件的文件名添加到列表中，形成一个包含所有以 .csv 结尾的文件名的列表。
+4. 因此，csv_files 变量将包含指定目录 input_dir 中所有以 .csv 结尾的文件的文件名列表。
+
 
--- remove_columns.py
+++ remove_columns.py
@@ -2,12 +2,17 @@ import csv
 import os
 
 def remove_columns(input_dir, output_dir):
+    
+    # 获取输入目录中的所有以 .csv 结尾的文件
     csv_files = [file for file in os.listdir(input_dir) if file.endswith('.csv')]
 
+
     for file in csv_files:
+        # 构建输入文件路径和输出文件路径
         input_path = os.path.join(input_dir, file)
         output_path = os.path.join(output_dir, file)
 
+        # 打开输入文件和输出文件
         with open(input_path, 'r', encoding='utf-8', newline='') as input_file, open(output_path, 'w', newline='') as output_file:
             reader = csv.reader(input_file)
             writer = csv.writer(output_file)
@@ -24,4 +29,5 @@ def remove_columns(input_dir, output_dir):
 input_directory = r'E:\test'  # 输入目录
 output_directory = r'E:\output'  # 输出目录
 
+# 调用函数进行列删除操作
 remove_columns(input_directory, output_directory)

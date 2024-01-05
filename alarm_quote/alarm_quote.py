1333217d3711100839811807475f525c8fb05a02
WenPeng.OuYang <101856244+Eddie-0721@users.noreply.github.com>
2024-01-05 16:11:25 +0800

    新增盘后15:05分自动退出程序，日志每日输出到新的文件

--- alarm_quote/alarm_quote.py
+++ alarm_quote/alarm_quote.py
@@ -1,12 +1,11 @@
 '''
-Author       : OuYang
-Date         : 2024-01-04 10:45:29
-Version      : v1.0
-LastEditors  : OuYang
-LastEditTime : 2024-01-05 13:18:47
-Description  : 使用WechatBot发送webhook消息-quote告警
+Author: OuYang
+Date: 2024-01-05 13:29:15
+LastEditors: OuYang
+LastEditTime: 2024-01-05 16:06:02
+version: v1.1
+Descripttion: 使用WechatBot发送webhook消息-yullog告警
 '''
-
 
 import send_webhook_tools
 import alarm_template_webhook as alarm_template_webhook
@@ -15,16 +14,32 @@ import platform
 import socket
 import inspect
 import logging
+from logging.handlers import RotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler
 import hashlib
 import time
 from datetime import datetime
 
+
+################### log config ###################
 # log_file 建议与send_webhook_tools类保持一致
-log_file = "/home/algo/quote/test/nohuo.log"
-logging.basicConfig(filename=log_file, level=logging.INFO, format='[%(asctime)s] - [%(levelname)s] - %(message)s')
+log_file = "/home/algo/shell/alarm_quote/nohup.log"
+
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.INFO)
+
+logging_handler = TimedRotatingFileHandler(log_file, when='midnight', encoding='utf-8')
+logging_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('[%(asctime)s] - [%(levelname)s] - %(message)s')
+logging_handler.setFormatter(formatter)
+
+logger.addHandler(logging_handler)
+################### END ###################
 
 
 def get_system_info():
+    
     # 获取os信息
     #os_name = platform.platform()
     # 获取os类型
@@ -64,6 +79,12 @@ def is_between_time(begin_time, end_time):
     else:
         return False
 
+def is_close_time(close_time):
+    now = time.strftime('%H:%M:%S')
+    if now >= close_time:
+        return True
+
+
 
 if __name__ == '__main__':
     # 正式环境
@@ -79,6 +100,7 @@ if __name__ == '__main__':
     Environment_name = "金桥环境"
     condition = "两分钟检查一次csv的MD5值是否相同，相同则发送告警"
     interval_time = 120
+    close_time = "15:05:00"
     ## end ##
 
     def check_file_md5(directory):
@@ -94,23 +116,26 @@ if __name__ == '__main__':
                         md5 = calculate_md5(file_path)
                         if file_path in md5_dict:
                             if md5_dict[file_path] == md5:
-                                logging.error(f"Error: MD5 of file {file_path} has not changed!")
+                                logger.error(f"Error: MD5 of file {file_path} has not changed!")
                                 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                                 bot.send_markdown(alarm_template_webhook.markdown_4(get_local_ip(),alarm_template_webhook.get_system_info(),Environment_name,current_time,procedure,file_path,condition))
                         md5_dict[file_path] = md5
-                        logging.info(f"File: {file_path}, MD5: {md5}")
+                        logger.info(f"File: {file_path}, MD5: {md5}")
                     if os.path.isfile(file_path) and file.endswith(".sh.csv"):
                         md5 = calculate_md5(file_path)
                         if file_path in md5_dict:
                             if md5_dict[file_path] == md5:
-                                logging.error(f"Error: MD5 of file {file_path} has not changed!")
+                                logger.error(f"Error: MD5 of file {file_path} has not changed!")
                                 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                                 bot.send_markdown(alarm_template_webhook.markdown_4(get_local_ip(),alarm_template_webhook.get_system_info(),Environment_name,current_time,procedure,file_path,condition))
                         md5_dict[file_path] = md5
-                        logging.info(f"File: {file_path}, MD5: {md5}")
+                        logger.info(f"File: {file_path}, MD5: {md5}")
                 nowtime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            logging.info(f"Time: {nowtime}")
+            elif is_close_time(close_time):
+                logger.info(f"The current time is {close_time}, exit the program normally!")
+                logger.info("Done")
+                exit(0)
+            logger.info(f"Time: {nowtime}")
             time.sleep(interval_time)
-        
 
     check_file_md5(market_path)
